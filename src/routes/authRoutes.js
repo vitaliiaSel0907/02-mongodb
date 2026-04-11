fe00c6af770be358baf55cf0bd1f9922989fc2b1
Vitaliia <vitaliia0907@gmail.com>
2026-04-11 11:48:15 +0200

    Modified files

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,29 +6,51 @@ import {
   loginUser,
   refreshUserSession,
   logoutUser,
+  requestResetEmail,
+  resetPassword,
 } from "../controllers/authController.js";
 
 import {
   registerUserSchema,
   loginUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
 } from "../validations/authValidation.js";
 
 const router = Router();
 
+//  REGISTER
 router.post(
   "/register",
   celebrate({ [Segments.BODY]: registerUserSchema }),
   registerUser
 );
 
+// LOGIN
 router.post(
   "/login",
   celebrate({ [Segments.BODY]: loginUserSchema }),
   loginUser
 );
 
+//  REFRESH
 router.post("/refresh", refreshUserSession);
 
+//  LOGOUT
 router.post("/logout", logoutUser);
+
+//  REQUEST RESET EMAIL
+router.post(
+  "/request-reset-email",
+  celebrate({ [Segments.BODY]: requestResetEmailSchema }),
+  requestResetEmail
+);
+
+//  RESET PASSWORD
+router.post(
+  "/reset-password",
+  celebrate({ [Segments.BODY]: resetPasswordSchema }),
+  resetPassword
+);
 
 export default router;
--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,13 +1,24 @@
- import Joi from "joi";
+import Joi from "joi";
 
+//  REGISTER
 export const registerUserSchema = Joi.object({
   email: Joi.string().email().required(),
-
   password: Joi.string().min(8).required(),
 });
 
+//  LOGIN
 export const loginUserSchema = Joi.object({
   email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
 
+//  REQUEST RESET EMAIL
+export const requestResetEmailSchema = Joi.object({
+  email: Joi.string().email().required(),
+});
+
+//  RESET PASSWORD
+export const resetPasswordSchema = Joi.object({
+  token: Joi.string().required(),
   password: Joi.string().required(),
 });
